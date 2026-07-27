1f19df498eb651f680b082656af9d886ced41d9f
farhad <ienanloofarhad@gmail.com>
2026-07-27 21:47:07 +0330

    farhad_usgs_catalog

--- src/collectors/usgs_collector.py
+++ src/collectors/usgs_collector.py
@@ -1,1 +1,25 @@
 # Collects earthquake data from USGS API
+from datetime import datetime, timedelta
+
+import requests
+
+end_date = datetime.now()
+start_date = end_date - timedelta(days=30)
+
+start_date = start_date.strftime("%Y-%m-%d")
+end_date = end_date.strftime("%Y-%m-%d")
+
+params = {
+    "format": "csv",
+    "starttime": start_date,
+    "endtime": end_date,
+    "minlatitude": 24,
+    "maxlatitude": 46,
+    "minlongitude": 123,
+    "maxlongitude": 146,
+    "minmagnitude": 1,
+}
+res = requests.get("https://earthquake.usgs.gov/fdsnws/event/1/query", params=params)
+
+with open("./data/raw/usgs.csv", "w", encoding="utf-8") as file:
+    file.write(res.text)
--- src/transform/usgs_transform.py
+++ src/transform/usgs_transform.py
@@ -1,1 +1,20 @@
 # Cleans/transforms USGS data
+
+import pandas as pd
+
+data_usgs = pd.read_csv("./data/raw/usgs.csv")
+
+data_usgs = data_usgs[
+    [
+        "time",
+        "latitude",
+        "longitude",
+        "depth",
+        "mag",
+        "place",
+    ]
+]
+data_usgs["source"] = "USGS"
+data_usgs.dropna(inplace=True)
+data_usgs.drop_duplicates(inplace=True)
+data_usgs.to_csv("./data/processed/JAPAN_USGS.csv", index=False)
