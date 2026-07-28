5599b9b46689a0b8a938ec4b6c347dc0ba585023
Bahar <baharmansourbeigi@gmail.com>
2026-07-28 20:36:48 +0330

    feat: add function

--- src/collectors/geofon_collector.py
+++ src/collectors/geofon_collector.py
@@ -1,80 +1,97 @@
+import requests
+import pandas as pd
+from bs4 import BeautifulSoup
 from datetime import datetime, timedelta
 
-import pandas as pd
-import requests
-from bs4 import BeautifulSoup
 
-end_date = datetime.today().date()
-start_date = end_date - timedelta(days=30)
+def extract_raw() -> pd.DataFrame:
+    end_date = datetime.today().date()
+    start_date = end_date - timedelta(days=30)
 
-url = "https://geofon.gfz.de/eqinfo/list.php"
+    url = "https://geofon.gfz.de/eqinfo/list.php"
 
-params = {
-    "datemin": str(start_date),
-    "datemax": str(end_date),
-    "latmax": 46,
-    "latmin": 24,
-    "lonmin": 123,
-    "lonmax": 146,
-    "magmin": "",
-    "fmt": "html",
-    "nmax": "",
-}
+    params = {
+        "datemin": str(start_date),
+        "datemax": str(end_date),
+        "latmax": 46,
+        "latmin": 24,
+        "lonmin": 123,
+        "lonmax": 146,
+        "magmin": "",
+        "fmt": "html",
+        "nmax": "",
+    }
 
+    rows = []
 
-rows = []
+    while True:
+        response = requests.get(url, params=params , timeout=10 )
 
-while True:
-    response = requests.get(url, params=params)
+        soup = BeautifulSoup(response.text, "html.parser")
 
-    soup = BeautifulSoup(response.text, "html.parser")
+        event_selector = "a[href*='event.php?id=']"
+        events = soup.select(event_selector)
 
-    event_selector = "a[href*='event.php?id=']"
-    events = soup.select(event_selector)
+        for event in events:
+            try:
+                magnitude_selector = "span[class='magbox']"
+                magnitude = event.select_one(magnitude_selector).text.strip()
 
-    for event in events:
-        try:
-            magnitude_selector = "span[class='magbox']"
-            magnitude = event.select_one(magnitude_selector).text.strip()
+                info_selector = "div[class='col-xs-12']"
+                info = event.select(info_selector)
 
-            info_selector = "div[class='col-xs-12']"
-            info = event.select(info_selector)
+                coordinate = info[0]["title"].split(",")
 
-            coordinate = info[0]["title"].split(",")
+                longitude = coordinate[0].strip()
+                latitude = coordinate[1].strip()
 
-            longitude = coordinate[0].strip()
-            latitude = coordinate[1].strip()
+                place = info[0].text.strip()
 
-            place = info[0].text.strip()
+                time = info[1].contents[0].strip()
 
-            time = info[1].contents[0].strip()
+                depth_selector = "span[class='pull-right']"
+                depth = info[1].select_one(depth_selector).text.strip()
 
-            depth_selector = "span[class='pull-right']"
-            depth = info[1].select_one(depth_selector).text.strip()
+                rows.append(
+                    {
+                        "time": time,
+                        "latitude": latitude,
+                        "longitude": longitude,
+                        "depth": depth,
+                        "mag": magnitude,
+                        "place": place,
+                    }
+                )
 
-            rows.append(
-                {
-                    "time": time,
-                    "magnitude": magnitude,
-                    "depth": depth,
-                    "latitude": latitude,
-                    "longitude": longitude,
-                    "place": place,
-                    "source": "GEOFON",
-                }
-            )
+            except Exception as error:
+                print(f"Error parsing one event: {error}")
+                continue
 
-        except Exception as error:
-            print(f"Error parsing one event: {error}")
-            continue
-
-    link = soup.select_one("span.pull-left a")
-    if not link:
-        break
-    url = "https://geofon.gfz.de/eqinfo/" + link["href"]
-    params = {}
+        link = soup.select_one("span.pull-left a")
+        if not link:
+            break
+        url = "https://geofon.gfz.de/eqinfo/" + link["href"]
+        params = {}
+        
+    return pd.DataFrame(rows)
 
 
-df = pd.DataFrame(rows)
+def export_raw(df: pd.DataFrame) -> None:
+    file_path = "data/raw/geofon.csv"
 
-df.to_csv("data/raw/geofon.csv", index=False, encoding="utf-8")
+    df.to_csv(file_path, index=False, encoding="utf-8")
+
+
+def load_raw() -> pd.DataFrame:
+    file_path = "data/raw/geofon.csv"
+
+    try:
+        return pd.read_csv(file_path ,encoding="utf-8")
+    except FileNotFoundError as e:
+        raise FileNotFoundError(f"Dataset file not found: '{file_path}'.") from e
+    except pd.errors.EmptyDataError as e:
+        raise ValueError(f"Dataset file is empty: '{file_path}'.") from e
+    except pd.errors.ParserError as e:
+        raise ValueError(
+            f"Dataset file could not be parsed as CSV: '{file_path}'."
+        ) from e
--- src/transform/geofon_transform.py
+++ src/transform/geofon_transform.py
@@ -2,33 +2,51 @@ import re
 import pandas as pd 
 from datetime import datetime
 
+def transform_genfon_data() -> pd.DataFrame:
+    raw_rows = pd.read_csv("data/raw/geofon.csv")
+    raw_rows = raw_rows.to_dict("records")
 
-raw_rows = pd.read_csv("data/raw/geofon.csv")
-raw_rows = raw_rows.to_dict("records")
+    processed_rows = []
 
-processed_rows = []
+    for row in raw_rows:
+        try:
+            longitude = float(row["longitude"].replace("°E", "").strip())
+            latitude = float(row["latitude"].replace("°N", "").strip())
 
-for row in raw_rows:
+            depth = re.search(r"\d+", row["depth"]).group()
+            
+            time = datetime.strptime(row["time"], "%Y-%m-%d %H:%M:%S.%f").strftime("%Y-%m-%d %H:%M:%S")
+
+            processed_rows.append({
+                "time": time ,
+                "latitude": latitude,
+                "longitude": longitude,
+                "depth": depth,
+                "mag": row["mag"],
+                "place": row["place"],
+                "source": "GEOFON",
+            })
+        except Exception as error:
+            print(f"Error processing event : {error}")
+            continue
+    return pd.DataFrame(processed_rows)
+
+def export_transformed(df: pd.DataFrame) -> None:
+    file_path = "data/processed/geofon.csv"
+
+    df.to_csv(file_path, index=False ,encoding="utf-8")
+
+
+def load_transformed() -> pd.DataFrame:
+    file_path = "data/processed/geofon.csv"
+
     try:
-        longitude = float(row["longitude"].replace("°E", "").strip())
-        latitude = float(row["latitude"].replace("°N", "").strip())
-
-        depth = re.search(r"\d+", row["depth"]).group()
-        
-        time = datetime.strptime(row["time"], "%Y-%m-%d %H:%M:%S.%f").strftime("%Y-%m-%d %H:%M:%S")
-
-        processed_rows.append({
-            "time": time ,
-            "mag": row["magnitude"],
-            "depth": depth,
-            "latitude": latitude,
-            "longitude": longitude,
-            "place": row["place"],
-            "source": "GEOFON",
-        })
-    except Exception as error:
-        print(f"Error processing event : {error}")
-        continue
-
-df_processed = pd.DataFrame(processed_rows)
-df_processed.to_csv("data/processed/geofon.csv", index=False, encoding="utf-8")
+        return pd.read_csv(file_path , encoding="utf-8")
+    except FileNotFoundError as e:
+            raise FileNotFoundError(f"Dataset file not found: '{file_path}'.") from e
+    except pd.errors.EmptyDataError as e:
+        raise ValueError(f"Dataset file is empty: '{file_path}'") from e
+    except pd.errors.ParserError as e:
+        raise ValueError(
+            f"Dataset file could not be parsed as CSV: '{file_path}'"
+        ) from e
