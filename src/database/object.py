53c6d3a5c7951c38ff5f0f4806164a9048e7ecea
Ilia Bahrebar <std.ilia.bahrebar@gmail.com>
2026-07-29 22:24:37 +0330

    refactor(database): use the sql directory to find scripts

--- src/database/object.py
+++ src/database/object.py
@@ -2,11 +2,12 @@ from __future__ import annotations
 
 from collections.abc import Iterator
 from contextlib import contextmanager
-from pathlib import Path
 from typing import NamedTuple
 
 import psycopg2
 from psycopg2.extras import NamedTupleCursor
+
+from config.settings import BASE_DIR
 
 
 class Database:
@@ -22,7 +23,7 @@ class Database:
         self._cursor.executemany(query, params)
 
     def run_script(self, script_path: str) -> None:
-        path = Path(script_path)
+        path = BASE_DIR / "sql" / script_path
         if not path.is_file():
             raise ValueError(f"Script file not found: '{path}'.")
         script = path.read_text()
