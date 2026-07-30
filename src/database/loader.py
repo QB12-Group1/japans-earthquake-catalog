98e7197e25e775b887962de7932ac588b129506b
Ilia Bahrebar <std.ilia.bahrebar@gmail.com>
2026-07-30 09:58:21 +0330

    feat: change the database data loader logic

--- src/database/loader.py
+++ src/database/loader.py
@@ -1,14 +1,6 @@
 import pandas as pd
-from sqlalchemy import text
 
 from src.database.session import get_session
-
-
-def drop_table() -> None:
-    with get_session() as session:
-        query = text("DROP TABLE IF EXISTS earthquakes")
-        session.execute(query)
-        session.commit()
 
 
 def load_to_sql(df: pd.DataFrame) -> None:
@@ -19,7 +11,7 @@ def load_to_sql(df: pd.DataFrame) -> None:
                 "earthquakes",
                 session.bind,
                 chunksize=100,
-                if_exists="append",
+                if_exists="replace",
                 index=False,
             )
         except Exception:
