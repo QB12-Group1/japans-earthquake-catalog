df7c7a58e1beca107f88d65cbd34debe46080b06
Bahar <baharmansourbeigi@gmail.com>
2026-07-31 21:44:13 +0330

    Increase average values precision to 2 decimal places

--- sql/analysis/grouped_analysis.sql
+++ sql/analysis/grouped_analysis.sql
@@ -2,7 +2,7 @@ SELECT month,
        region ,
        category,
        COUNT(*),
-       round(AVG(mag)::NUMERIC , 1)::FLOAT as avg_mag,
-       round(AVG(depth)::NUMERIC , 1)::FLOAT as avg_depth
+       round(AVG(mag)::NUMERIC , 2)::FLOAT as avg_mag,
+       round(AVG(depth)::NUMERIC , 2)::FLOAT as avg_depth
 FROM earthquakes
 GROUP BY month , region , category;
